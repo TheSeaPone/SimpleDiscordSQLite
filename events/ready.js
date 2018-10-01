b9aa8b8d4e767d8c57f23bbfae77b8d6a8481fb4
Jengas <timaiv2@gmail.com>
2018-10-01 12:13:31 +0200

    Config and playing status

--- events/ready.js
+++ events/ready.js
@@ -5,4 +5,6 @@ module.exports = (client, message) => {
   const dbsql = client.dbsql;
 
   db.prepare('CREATE TABLE IF NOT EXISTS guildmember ( userid TEXT, data TEXT )').run();
+
+  client.user.setActivity(`${client.config.game.activity}`, { type: `${client.config.game.status}` });
 };
